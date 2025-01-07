ba64826a49a24dc20ff42dbcaff261f2df60f97d
Dhruv <makotoisamu16@gmail.com>
2025-01-08 00:21:11 +0530

    changed frontend url

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://baatkaro.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }));
 
